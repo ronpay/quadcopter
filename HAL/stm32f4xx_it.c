2474748268820ab1cdff4139a85171829d68bc82
ronpay <sh3652@163.com>
2021-06-07 17:08:49 +0800

    fix ISR

--- HAL/stm32f4xx_it.c
+++ HAL/stm32f4xx_it.c
@@ -6,19 +6,30 @@
 
 
 void USART6_IRQHandler(void) {
+#if OS_CRITICAL_METHOD == 3u                               /* Allocate storage for CPU status register */
+    OS_CPU_SR  cpu_sr = 0u;
+#endif
+	OS_ENTER_CRITICAL();
 	OSIntEnter();
+	OS_EXIT_CRITICAL();
 	
 	HM10_IRQ_IRQHandler();
 	
-	OSIntExit();   
+	OSIntExit(); 
+	
 }
 
 void TIM3_IRQHandler(void) {
+#if OS_CRITICAL_METHOD == 3u                               /* Allocate storage for CPU status register */
+    OS_CPU_SR  cpu_sr = 0u;
+#endif
+	OS_ENTER_CRITICAL();
 	OSIntEnter();
+	OS_EXIT_CRITICAL();
 	
 	Receiver_IRQ_Handler();
 	
-	OSIntExit();   
+	OSIntExit();
 }
 
 void HardFault_Handler(void) {
